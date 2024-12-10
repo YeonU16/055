93ccfc8a6d09ee6b513ef9ac6d73fb87acf5bc35
YEONU-\yeonu <39lolo@naver.com>
2024-12-11 00:13:47 +0900

    두 번째 커밋 화면 크기 및 고양이, 진공청소기 아트 정의 화면의 너비와 높이, 게임 캐릭터의 너비와 높이를 각각의 비율에 맞게 설정함

---
+++ 고양이 게임/고양이 게임/고양이 게임.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+#include <ctype.h>
+
+// 화면크기, 고양이와 진공 청소기 크기 정의
+#define WIDTH 120      // 화면 너비
+#define HEIGHT 25      // 화면 높이
+#define CAT_WIDTH 7    // 고양이 아트 너비
+#define CAT_HEIGHT 3   // 고양이 아트 높이
+#define MAX_VACUUMS 10 // 진공청소기 최대 수
